508a7c1abd8e257b1d1eadfebe46efd0f79c86a4
Ubuntu <ubuntu@ip-172-31-29-243.ec2.internal>
2025-10-24 14:42:09 +0000

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = process.env.PORT || 80;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from my CI/CD demo! Version: ' + (process.env.IMAGE_TAG || 'dev') + '\n');
+  res.end('Hello from updated CI/CD demo!\n');
 });
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
